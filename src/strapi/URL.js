267a0fdba3cbbc627241c653c8652edea6eb6782
Branca Chiotte <brancachiotte@gmail.com>
2020-11-10 12:06:08 +0000

    done

--- src/strapi/URL.js
+++ src/strapi/URL.js
@@ -1,2 +1,2 @@
-export default 'http://localhost:1337'
-// export default "https://svelte-store-server.herokuapp.com";
+//export default 'http://localhost:1337'
+export default "https://svelte-razor-app-server.herokuapp.com";
